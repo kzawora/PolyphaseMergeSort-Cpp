1c4c1a9c557b3e1e026d0c5329e627de27c2961f
kzawora <konrad.zawora@outlook.com>
2018-10-23 22:42:04 +0200

    modify test case

--- main.cpp
+++ main.cpp
@@ -10,20 +10,29 @@ int main()
 	tape1.GenerateTape(size);
 
 	tape1.OpenStream();
-	std::vector<Record> recs;
-	while (recs.size() < size)
+	std::vector<std::vector<Record>> recs;
+	do
+		recs.push_back(tape1.RecordBlockRead());
+	while (recs.back().size() != 0);
+	recs.pop_back();
+
+	int counter = 0;
+	for (auto record : recs)
 	{
-		auto temp = tape1.RecordBlockRead();
-		recs.insert(recs.end(), temp.begin(), temp.end());
+		counter++;
+		long long size = 0;
+		for (auto val : record)
+			size += (val.values.size() + 1) * sizeof(double);
+		std::cout << "==================================== BLOCK: " << counter << ", RECORDS: " << record.size() << " (" << size << " bytes) "
+				  << " ====================================" << std::endl;
+		for (auto val : record)
+			std::cout << val;
 	}
-	for (auto val : recs)
-		std::cout << val;
-
 	tape1.CloseStream();
-
+	/*
 	Record r1({1., 2., 3., 999999.});
 	Record r2({.1, 2., 4., 6., 1., 2., 6., 1060., 2.});
 	std::cout << (r1 > r2) << std::endl;
-
+*/
 	return 0;
 }
